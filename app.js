c431693239d1b3d81ae96c161a69a45bb3bf6f3f
lwren22 <76826350+lwren22@users.noreply.github.com>
2021-03-23 18:08:17 +0000

    jasmins bit

--- app.js
+++ app.js
@@ -14,3 +14,29 @@ navBarToggle.addEventListener("click", function () {
         var userEmail = prompt("Enter your email address here!");
         alert ("Thank you!");
     }
+
+var modal = document.getElementById("modalBox");
+
+// Get the button that opens the modal
+var left = document.getElementsByClassName("subFav");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+document.getElementById("left").addEventListener("mouseover", function(event) {
+    document.getElementById("left").color = "red";
+    console.log("event", event.target);
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  	modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+};
