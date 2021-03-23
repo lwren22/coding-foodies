62cf91211c62d0b580cebb967cd9fa65f1bc8a7f
lwren22 <76826350+lwren22@users.noreply.github.com>
2021-03-23 18:52:06 +0000

    last bits

--- app.js
+++ app.js
@@ -15,28 +15,27 @@ navBarToggle.addEventListener("click", function () {
         alert ("Thank you!");
     }
 
-var modal = document.getElementById("modalBox");
+    var modal = document.getElementById("modalBox");
 
-// Get the button that opens the modal
-var left = document.getElementsByClassName("subFav");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-document.getElementById("left").addEventListener("mouseover", function(event) {
-    document.getElementById("left").color = "red";
-    console.log("event", event.target);
-});
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  	modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-};
+    // Get the button that opens the modal
+    var left = document.getElementById("left");
+    console.log(left);
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    left.onclick= function() {
+        modal.style.display = "block";
+    };
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+          modal.style.display = "none";
+    };
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
