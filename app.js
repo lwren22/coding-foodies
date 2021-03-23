94cbb496b36d7250ea608ad83e3a11eb228c14ba
lwren22 <76826350+lwren22@users.noreply.github.com>
2021-03-23 16:03:39 +0000

    Mollys part

--- app.js
+++ app.js
@@ -1,42 +1,3 @@
-// window.onload = rotate;
-
-// var thePic = 0;
-// var pics = ["pics/cupcakes.png","pics/goulash.png","pics/eclair.png","pics/pizza.png"];
-
-
-// document.getElementById("rightBtn").addEventListener("click", nextPic);
-// document.getElementById("leftBtn").addEventListener("click", previousPic);
-
-
-// function nextPic() 
-// {
-//     thePic++;
-//     if (thePic == pics.length) {
-//        thePic = 0;
-//     }
-//     document.getElementById("fourPics").src = pics[thePic]; 
-//     }
-
-
-// function previousPic() 
-// {
-//     thePic--;
-//     if (thePic < 0) {
-//         thePic = 3;  
-//     } 
-//     document.getElementById("fourPics").src = pics[thePic]; 
-//         }
-
-// function rotate() {
-//     thePic++;
-//     if (thePic == pics.length) {
-//        thePic = 0;
-//     }
-//     document.getElementById("fourPics").src = pics[thePic]; 
-
-//      setTimeout(rotate, 5 * 1000);
-// }
-
 let mainNav = document.getElementById("jsMenu");
 let navBarToggle = document.getElementById("jsNavBarToggle");
 
@@ -45,4 +6,11 @@ navBarToggle.addEventListener("click", function () {
     mainNav.classList.toggle("active");
 });
 
+/* var newsletter 
+    prompt("Enter your email address here!");
+    returnprompt("Thank you!"); */
 
+    function promptMe(){
+        var userEmail = prompt("Enter your email address here!");
+        alert ("Thank you!");
+    }
